c108557f05b3c564a81f068b6e824491d85eeab7
Andrey Mitin <andrey.mitin@bigtincan.com>
2025-12-06 18:04:18 -0600

    Make sure the repo is up-to-date on every invocation of readme.sh

--- readme.sh
+++ readme.sh
@@ -1,5 +1,9 @@
+git pull
+
 # pair every backlink with a forward link in the Next section of the backlinked doc
 ggrep -rH -oP '(?<=\.\./\.\./\.\./)2025[^ ]*md' 2025 | awk -f filter_singly_linked.awk | sort | python3.13 next.py
+
 git add . && git commit -m "Linking" && git push
+
 sh find_roots.sh | python3.13 readme.py
-git diff
+git diff # see if there are new roots to explain in readme.md
