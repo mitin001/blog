145008abd5e8c65d2246a7100fa3662cbb212e73
Andrey Mitin <andrey.mitin@bigtincan.com>
2025-12-20 00:23:41 -0600

    Reclassify the questions from Sep 2025 by academic discipline

--- faq/readme.sh
+++ faq/readme.sh
@@ -3,7 +3,10 @@ grep -r '^## ' 2025/09/[23]* | awk '{print NR":## "$0}' > faq/txt/2025-09-2.txt
 
 cat faq/prompt.txt > faq/prompts/2025-09-2.txt
 cat faq/txt/2025-09-2.txt | awk -F':## ' '{print $1, $3}' >> faq/prompts/2025-09-2.txt
-# send faq/prompts/2025-09-2.txt to chatgpt and get back classifications/2025-09-2.json
+cat faq/prompts/2025-09-2.txt | pbcopy
+# send faq/prompts/2025-09-2.txt to chatgpt and get back faq/classifications/2025-09-2.json
+subl faq/classifications/2025-09-2.json
+
 cat faq/txt/2025-09-2.txt | awk -F':## ' '{print $1"\t"$2"\t"$3}' | jq -Rn '[inputs | split("\t") | { (.[0]): .[1:] }] | add' > faq/json/2025-09-2.json
 
 cd faq
