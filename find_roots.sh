8c5a1de5b9eff5a85cdc053bb757a5c0b625d260
Andrey Mitin <andrey.mitin@showpad.com>
2026-03-03 23:29:22 -0600

    Linking

--- find_roots.sh
+++ find_roots.sh
@@ -1,3 +1,3 @@
 for f in 202[56]/*/*/*; do
-  [ -f "$f" ] && ! gsed '1b; /^#/q; p' "$f" | grep -q '../../..' && echo "$f"
+  [ -f "$f" ] && ! gsed '1b; /^#/q; p' "$f" | grep -F -q '../../..' && echo "$f"
 done
